3a1baf00ae341415a8872c50d8d6f74fedd28623
Javeria Jawed <javeriajawed23@gmail.com>
2025-09-10 04:32:12 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import Link from "next/link";
 import Image from "next/image";
 import { ArrowRight, MapPin, Star, Users, Mail } from "lucide-react";
